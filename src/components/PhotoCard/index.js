977f2b51bf1f8490a28f5d7fbc686c482af6c64c
Shherardd <Gerardwjones@gmail.com>
2020-08-30 02:30:56 -0500

    PhotoCard and react icons

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,18 +1,20 @@
 import React from 'react'
+import { ImgWrapper, Img, Button } from './styles'
+import { MdFavoriteBorder } from 'react-icons/md'
 
 const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1507146426996-ef05306b995a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
-export const PhotoCard = ({ id, likes = 0, src = D }) => {
+export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
   return (
     <article>
-      <a href={`/detal/${id}`}>
-        <div>
-          <img src={src} />
-        </div>
+      <a href={`/detail/${id}`}>
+        <ImgWrapper>
+          <Img src={src} />
+        </ImgWrapper>
       </a>
 
-      <button>
-        {likes} Likes!
-      </button>
+      <Button>
+        <MdFavoriteBorder size='32px' /> {likes} Likes!
+      </Button>
     </article>
   )
 }
