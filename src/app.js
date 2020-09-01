e35399cb13505339e50523f2b5f05aa14c4f83b5
Shherardd <Gerardwjones@gmail.com>
2020-08-31 22:54:40 -0500

    SVG logo to react Component

--- src/app.js
+++ src/app.js
@@ -2,8 +2,10 @@ import React from 'react'
 import { ListOfCategories } from './components/ListOfCategories'
 import { GlobalStyle } from './GlobalStyles'
 import { ListOfPhotoCards } from './components/ListOfPhotoCards'
+import Logo from './components/Logo'
 const App = () => (
   <>
+    <Logo />
     <GlobalStyle />
     <ListOfCategories />
     <ListOfPhotoCards />
