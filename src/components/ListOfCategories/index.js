9c183cc2e57e288e322353f0dff359af180cd77f
Shherardd <Gerardwjones@gmail.com>
2020-09-01 21:57:38 -0500

    Fetch to categories

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Category } from '../Category'
 import { List, Item } from './styles'
-import { categories } from '../../../api/db.json'
 export const ListOfCategories = () => {
+  const [categories, setCategories] = useState(mockCategories)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await window.fetch('https://bet-server.vercel.app/categories')
+      const data = await response.json()
+      setCategories(data)
+    }
+    fetchData()
+  })
+
   return (
     <List>
       {
