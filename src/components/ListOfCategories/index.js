07fc9c444467d567e4ed428f2cef52e9ff83c258
Shherardd <Gerardwjones@gmail.com>
2020-09-03 05:38:03 -0500

    custom hook isLoading Categories fetch

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,19 +1,27 @@
 import React, { useState, useEffect } from 'react'
 import { Category } from '../Category'
 import { List, Item } from './styles'
-export const ListOfCategories = () => {
+
+function useCategoriesData () {
   const [categories, setCategories] = useState([])
-  const [showFixed, setShowFixed] = useState(false)
+  const [loading, setLoading] = useState(false)
 
   useEffect(() => {
     const fetchData = async () => {
+      setLoading(true)
       const response = await window.fetch('https://bet-server.vercel.app/categories')
       const data = await response.json()
       setCategories(data)
+      setLoading(false)
     }
     fetchData()
   }, [])
 
+  return { categories, loading }
+}
+export const ListOfCategories = () => {
+  const [showFixed, setShowFixed] = useState(false)
+  const { categories, loading } = useCategoriesData()
   useEffect(function () {
     const onScroll = e => {
       console.log('scrolled')
@@ -31,7 +39,9 @@ export const ListOfCategories = () => {
         categories.map(category => <Item key={category.id}><Category {...category} /></Item>)
       }
     </List>
-
+  if (loading) {
+    return 'cargando...'
+  }
   return (
     <>
       {renderList()}
