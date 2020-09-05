20ba0fb2a7c61de070bad7fe9f23580026ddd803
Shherardd <Gerardwjones@gmail.com>
2020-09-05 06:43:02 -0500

    fetchin data w/ graphql and react-apollo

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import React from 'react'
 import reactDOM from 'react-dom'
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from 'react-apollo'
 import App from './app'
 
 const appDOM = document.getElementById('app')
 
-reactDOM.render(<App />, appDOM)
+const client = new ApolloClient({
+  uri: 'https://bet-server.vercel.app/graphql'
+})
+
+reactDOM.render(
+  <ApolloProvider client={client}><App /></ApolloProvider>
+  , appDOM)
