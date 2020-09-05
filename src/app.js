41f4fa505e3e082551bdaf63bfec4a5e16efc967
Shherardd <Gerardwjones@gmail.com>
2020-09-05 07:05:45 -0500

    high order component, container, and passing parameter from hoc to graphql query

--- src/app.js
+++ src/app.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import { ListOfCategories } from './components/ListOfCategories'
 import { GlobalStyle } from './styles/GlobalStyles'
-import { ListOfPhotoCards } from './components/ListOfPhotoCards'
+import { ListOfPhotoCards } from './container/ListOfPhotoCards'
 import { Logo } from './components/Logo'
 const App = () => (
   <>
     <Logo />
     <GlobalStyle />
     <ListOfCategories />
-    <ListOfPhotoCards />
+    <ListOfPhotoCards categoryId={2} />
   </>
 )
 
