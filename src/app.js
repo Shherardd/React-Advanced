194638b96ce20e903720626d3b148823552a30d5
Shherardd <Gerardwjones@gmail.com>
2020-09-05 23:37:03 -0500

    render props with Query from React-apollo

--- src/app.js
+++ src/app.js
@@ -2,14 +2,26 @@ import React from 'react'
 import { ListOfCategories } from './components/ListOfCategories'
 import { GlobalStyle } from './styles/GlobalStyles'
 import { ListOfPhotoCards } from './container/ListOfPhotoCards'
+import { PhotoCardWithQuery } from './container/PhotoCardWithQuery'
 import { Logo } from './components/Logo'
-const App = () => (
-  <>
-    <Logo />
-    <GlobalStyle />
-    <ListOfCategories />
-    <ListOfPhotoCards categoryId={2} />
-  </>
-)
+const App = () => {
+  const urlParams = new window.URLSearchParams(window.location.search)
+  const detailId = urlParams.get('detail')
+  // console.log(detailId)
+  return (
+    <>
+      <GlobalStyle />
+      <Logo />
+      {
+        detailId
+          ? <PhotoCardWithQuery id={detailId} />
+          : <>
+            <ListOfCategories />
+            <ListOfPhotoCards categoryId={2} />
+          </>
+      }
+    </>
+  )
+}
 
 export default App
