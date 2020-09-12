4c6637c9e4488aa786eb38c71b29e3c273ef91b8
Shherardd <Gerardwjones@gmail.com>
2020-09-11 21:03:23 -0500

    protected routes, w/ renderprops

--- src/app.js
+++ src/app.js
@@ -5,9 +5,15 @@ import { Logo } from './components/Logo'
 import { Home } from './pages/home'
 import { Detail } from './pages/detail'
 import { NavBar } from './components/NavBar'
+import { Favs } from './pages/Favs'
+import { User } from './pages/User'
+import { UnregisteredUser } from './pages/UnregisteredUser'
+const UserLogged = ({ children }) => {
+  return children({ isAuth: false })
+}
 const App = () => {
-  const urlParams = new window.URLSearchParams(window.location.search)
-  const detailId = urlParams.get('detail')
+  // const urlParams = new window.URLSearchParams(window.location.search)
+  // const detailId = urlParams.get('detail')
   // console.log(window.location.search)  --> empty string if there isnt qury into searchbar
   // console.log(detailId)
   return (
@@ -19,6 +25,21 @@ const App = () => {
         <Home path='/pet/:od' />
         <Detail path='/detail/:detailId' />
       </Router>
+      <UserLogged>
+        {
+          ({ isAuth }) => (
+            isAuth
+              ? <Router>
+                <Favs path='/favs' />
+                <User path='/user' />
+              </Router>
+              : <Router>
+                <UnregisteredUser path='/favs' />
+                <UnregisteredUser path='/user' />
+              </Router>
+          )
+        }
+      </UserLogged>
       <NavBar />
     </>
   )
---
+++ src/pages/UnregisteredUser.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export const UnregisteredUser = () => (
+  <h1>No registrado</h1>
+)
